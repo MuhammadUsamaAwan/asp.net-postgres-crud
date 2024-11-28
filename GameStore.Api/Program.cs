2c201e76c1a8183117d7718476f966cc15e235da
MuhammadUsamaAwan <underusama@gmail.com>
2024-11-28 21:30:46 +0500

    add postgres

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,14 @@
+using GameStore.Api.Data;
+using GameStore.Api.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
+
+builder.Services.AddNpgsql<GameStoreContext>(connectionString);
+
+var app = builder.Build();
+
+app.MapGamesEndpoints();
+
+app.Run();
